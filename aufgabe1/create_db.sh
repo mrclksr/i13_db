e571db0f4bf2f10469bd7c5e80fdadf74f35a05e
Marcel Kaiser <mk@freeshell.de>
2025-11-07 08:47:37 +0100

    Refactor create_db.sh to improve readability and structure of table creation scripts

--- aufgabe1/create_db.sh
+++ aufgabe1/create_db.sh
@@ -1,89 +1,96 @@
 #!/bin/sh
-#
-#
-#  +-KUNDEN----------------------------+
-#  \ kundennummer | vorname ... | land | 
-#   +----------------------------------+
-         
 
+CSV_SEPARATOR=';'
+ORDERED_TABLES="bestellung warenkorb produkt hersteller kunden"
+
+init_import_params() {
+cat << END
+.mode csv
+.separator ${CSV_SEPARATOR}
+
+END
+}
 
 create_kunden_db() {
 cat << END
-CREATE TABLE kunden (kundennummer TEXT PRIMARY KEY, vorname TEXT, nachname TEXT, strasse TEXT, hausnummer TEXT, plz TEXT, stadt TEXT, land TEXT);
-.mode csv
-.separator ;
+CREATE TABLE kunden (kundennummer TEXT PRIMARY KEY,
+vorname TEXT,
+nachname TEXT,
+strasse TEXT,
+hausnummer TEXT,
+plz TEXT,
+stadt TEXT,
+land TEXT);
 .import kunden.CSV kunden
+
 END
 }
 
 create_hersteller_db() {
 cat << END
 CREATE TABLE hersteller (name TEXT PRIMARY KEY, land TEXT);
-.mode csv
-.separator ;
 .import hersteller.CSV hersteller
-END
 
+END
 }
 
 create_produkte_db() {
 cat << END
-CREATE TABLE produkte (produktnummer TEXT PRIMARY KEY, bezeichnung TEXT, hersteller TEXT, FOREIGN KEY (hersteller) REFERENCES hersteller(name));
-.mode csv
-.separator ;
+CREATE TABLE produkte (produktnummer TEXT PRIMARY KEY,
+bezeichnung TEXT,
+hersteller TEXT,
+FOREIGN KEY (hersteller) REFERENCES hersteller(name));
 .import produkte.CSV produkte
+
 END
 }
 
 create_warenkorb_db() {
 cat << END
-CREATE TABLE warenkorb (bestellnummer INTEGER, produktnummer TEXT, menge INTEGER, preis REAL, PRIMARY KEY(bestellnummer, produktnummer), FOREIGN KEY (produktnummer) REFERENCES produkte(produktnummer));
-.mode csv
-.separator ;
+CREATE TABLE warenkorb (bestellnummer INTEGER,
+produktnummer TEXT,
+menge INTEGER,
+preis REAL,
+PRIMARY KEY(bestellnummer, produktnummer),
+FOREIGN KEY (produktnummer) REFERENCES produkte(produktnummer));
 .import warenkorb.CSV warenkorb
+
 END
 }
 
 create_bestellung_db() {
 cat << END
-CREATE TABLE bestellung (bestellnummer INTEGER PRIMARY KEY, datum TEXT, kundennummer TEXT, versandkosten REAL, gesamtkosten REAL, FOREIGN KEY (bestellnummer) REFERENCES warenkorb(bestellnummer), FOREIGN KEY (kundennummer) REFERENCES kunden(kundennummer));
-.mode csv
-.separator ;
+CREATE TABLE bestellung (bestellnummer INTEGER PRIMARY KEY,
+datum TEXT, kundennummer TEXT,
+versandkosten REAL,
+gesamtkosten REAL,
+FOREIGN KEY (bestellnummer) REFERENCES warenkorb(bestellnummer),
+FOREIGN KEY (kundennummer) REFERENCES kunden(kundennummer));
 .import bestellung.CSV bestellung
+
 END
 }
-
 
 save_db() {
 cat << END
 .save shop.db
-END
-}
 
-show_db() {
-cat << END
-SELECT * FROM kunden;
 END
 }
 
 drop_dbs() {
-cat << END
-DROP TABLE bestellung;
-DROP TABLE warenkorb;
-DROP TABLE produkte;
-DROP TABLE hersteller;
-DROP TABLE kunden;
-END
+    for t in ${ORDERED_TABLES}; do
+        echo "DROP TABLE IF EXISTS $t;"
+    done
 }
 
+create_tables() {
+	for table in ${ORDERED_TABLES}; do
+    	create_${table}_db
+	done
+}
+
+init
 drop_dbs
-
-create_kunden_db
-create_hersteller_db
-create_produkte_db
-create_warenkorb_db
-create_bestellung_db
+create_tables
 save_db
-show_db
-
-
